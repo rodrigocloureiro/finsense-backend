e258e88678ea9a04976a5ba4837dcef785d5ffcd
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-04-05 22:28:45 -0300

    base do DespesaController criada

---
+++ src/controllers/DespesaController.js
@@ -0,0 +1,13 @@
+class DespesaController {
+  async index(req, res) {}
+
+  async show(req, res) {}
+
+  async store(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
+
+export default new DespesaController();
