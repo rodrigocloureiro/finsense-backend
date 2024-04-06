0ab45004363105067385f4bf8dd37a420c2afe45
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-04-05 23:19:20 -0300

    refactor: DespesasRepository retornando apenas despesas

--- src/repositories/DespesasRepository.js
+++ src/repositories/DespesasRepository.js
@@ -1,12 +1,15 @@
 import dados from '../mocks/dados.js';
+import categorias from '../enums/categorias.js';
 
 class DespesasRepository {
   async findAll() {
-    return dados;
+    return dados.filter((dado) => dado.categoria === categorias.DESPESA);
   }
 
   async findById(id) {
-    const [despesa] = dados.filter(dado => dado.id === Number(id));
+    const [despesa] = dados.filter(
+      (dado) => dado.id === Number(id) && dado.categoria === categorias.DESPESA
+    );
     return despesa;
   }
 
